c94d58277b963883f7f60dfd39f5931c02dd4cf9
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-12 22:02:59 -0800

    student controler and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+jessie = Student.create(name: "Jessie")
+amy = Student.create(name: "Amy")
+lee = Student.create(name: "Lee")
