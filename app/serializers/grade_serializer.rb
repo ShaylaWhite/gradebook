36697a98ca98973658b8596db740bc5179d5eede
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-13 15:19:27 -0800

    studentcontroller

--- app/serializers/grade_serializer.rb
+++ app/serializers/grade_serializer.rb
@@ -1,4 +1,4 @@
 class GradeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name_of_class, :student_grade, :student_id
+  attributes :name_of_class, :student_grade, :student_id, :student
 end
