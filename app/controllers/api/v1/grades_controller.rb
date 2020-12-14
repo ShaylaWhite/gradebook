efefad81e8b165f086897761864e0a1244a5d568
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-13 22:53:31 -0800

    updateded

--- app/controllers/api/v1/grades_controller.rb
+++ app/controllers/api/v1/grades_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::GradesController < ApplicationController
 
     def index
-        @grades = Grade.all
-        render json: Grade.Serializaer.new(@grades)
+        grades = Grade.all
+        render json: GradeSerializaer.new(grades)
     end
 
 
