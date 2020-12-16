c14c3aea114af34d3e910658a9292149259e0d84
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-15 21:35:46 -0800

    added grade params

--- app/controllers/api/v1/grades_controller.rb
+++ app/controllers/api/v1/grades_controller.rb
@@ -1,25 +1,25 @@
 class Api::V1::GradesController < ApplicationController
   def index
-    @grades = Grade.all
+    grades = Grade.all
     options = {
       # include associated category
       include: [:student]
     }
     # pass options object to serializer
-    render json: GradeSerializer.new(@grades, options)
+    render json: GradeSerializer.new(grades, options)
   end
 
 
-def create
-
-  grade = Grade.new(grades_params)
-  if Grade.save
-
-    render json: grade, status: :accepted
-  else
-    render json: {errors: grade.errors.full_messages}, status: :unprocessible_entity
+  
+  def create
+    grade = Grade.new(grade_params)
+   grade.save
+  render json: GradeSerializer.new(grade), status: :accepted
   end
-end
+
+  def destroy 
+    grade = 
+  end 
 
 
 private
@@ -28,5 +28,9 @@ def grade_params
   params.require(:grade).permit(:name_of_class, :student_grade, :student_id)
 
 end
+
+def find_grade
+  @grade = Grade.find(params[:id])
+end 
 
 end
