5980483fc9113e4063be8991361517bf95354e00
ShaylaWhite <shayladwhite1@gmail.com>
2020-12-21 18:43:49 -0800

    added delete method

--- app/controllers/api/v1/grades_controller.rb
+++ app/controllers/api/v1/grades_controller.rb
@@ -1,26 +1,22 @@
 class Api::V1::GradesController < ApplicationController
   
   def index
-    @grade = Grade.all
-    options = {
-      # include associated student
-      include: [:student]
-    }
-    # pass options object to serializer
-    render json: GradeSerializer.new(@grade, options)
+    grades = Grade.all
+   
+    render json: GradeSerializer.new(grades)
   end
 
-  
+
   def create
     grade = Grade.new(grade_params)
     grade.save
-    render json: GradeSerializer.new(grade), status: :accepted
+      render json: GradeSerializer.new(grade), status: :accepted
+  
   end
 
-  def destroy 
-    grade = Grade.find_by(id:params[:id])
-    grade.destroy
-    render json: grade
+  def destroy
+     grade = Grade.find_by(id: params[:id])
+     grade.destroy
   end 
 
 
@@ -30,8 +26,6 @@ class Api::V1::GradesController < ApplicationController
     params.require(:grade).permit(:name_of_class, :student_grade, :student_id)
   end
 
-  def find_grade
-    @grade = Grade.find(params[:id])
-  end 
+
 
 end
